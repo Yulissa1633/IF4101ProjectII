bacf790a76cd411333658e90b5c6602e02bb2dca
Yulissa1633 <yulissa@1633gmail.com>
2021-06-19 12:32:11 -0600

    test18

--- client-angular/client-angular-project/server.js
+++ client-angular/client-angular-project/server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/dist/client-angular-project'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/client-angular-project/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/client-angular-project/src/app/app.component.html'));
 });
 
 // default Heroku port
